26a9febc4d7b5c601a586a5d64ce93ccbf47dd88
everlastingspring <prashanthrevallar03@gmail.com>
2024-06-28 01:08:18 +0530

    using mysql in microservices

---
+++ v3-spring-boot-app/accounts/src/main/java/com/techno_bank/accounts/service/IAccountService.java
@@ -0,0 +1,14 @@
+package com.techno_bank.accounts.service;
+
+import com.techno_bank.accounts.dto.CustomerDto;
+
+public interface IAccountService {
+
+    void createAccount(CustomerDto customerDto);
+
+    CustomerDto fetchAccount(String mobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
+}
