c8122625aeef97eb3a0a61abb835cc0327a3fb0c
everlastingspring <prashanthrevallar03@gmail.com>
2024-06-27 18:51:31 +0530

    Added: reading configurations from classpath, system path & remote server like git with qa & prod environments

---
+++ v2-spring-cloud-config/accounts/src/main/java/com/techno_bank/accounts/entity/Account.java
@@ -0,0 +1,30 @@
+package com.techno_bank.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Account extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
