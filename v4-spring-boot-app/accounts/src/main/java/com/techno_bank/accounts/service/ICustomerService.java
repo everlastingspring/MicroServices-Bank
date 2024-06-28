1cb90e6fae4cee446eaecd24c266cf1f1ecb334d
everlastingspring <prashanthrevallar03@gmail.com>
2024-06-28 22:41:07 +0530

    added eureka: client side service discovery & load balancing

---
+++ v4-spring-boot-app/accounts/src/main/java/com/techno_bank/accounts/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.techno_bank.accounts.service;
+
+import com.techno_bank.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
