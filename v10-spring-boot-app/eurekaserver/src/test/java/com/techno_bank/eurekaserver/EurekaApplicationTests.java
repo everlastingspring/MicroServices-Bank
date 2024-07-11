8cb591487006c59499b689cb0bc50b4fa4d68233
everlastingspring <prashanthrevallar03@gmail.com>
2024-07-11 22:55:39 +0530

    Event Driven microservices using KAFKA,Spring Cloud Functions & Stream

---
+++ v10-spring-boot-app/eurekaserver/src/test/java/com/techno_bank/eurekaserver/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techno_bank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ v10-spring-boot-app/message/src/main/java/com/techno_bank/message/dto/AccountsMessageDto.java
@@ -0,0 +1,10 @@
+package com.techno_bank.message.dto;
+
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMessageDto(Long accountNumber, String name, String email, String mobileNumber) {
+}
